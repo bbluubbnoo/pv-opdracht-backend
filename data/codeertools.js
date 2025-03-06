e15372c98035c1d96526c90ff8eabb18b672b496
Valcked <davidvalcke@hotmail.be>
2025-03-06 01:52:57 +0100

    My commit without node_modules and changing data and routes

---
+++ data/codeertools.js
@@ -0,0 +1,7 @@
+const codeertools = [
+    { id: 1, name: "Visual Studio Code", description: "Gratis editor van Microsoft.", price: "Gratis" },
+    { id: 2, name: "IntelliJ IDEA", description: "Krachtige Java IDE.", price: "€149 per jaar" },
+    { id: 3, name: "Sublime Text", description: "Minimalistische code editor.", price: "€80 eenmalig" }
+];
+
+module.exports = codeertools;
