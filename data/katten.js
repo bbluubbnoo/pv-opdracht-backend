081c0709f49d65d299ca4282a6890811a03c6b2d
fatima146 <fatima.salamova@student.vives.be>
2025-03-10 18:52:53 +0100

    kat dummy data

---
+++ data/katten.js
@@ -0,0 +1,7 @@
+const cats = [
+    { id: 1, name: "Whiskers", breed: "Siamese", price: "€ 2000" },
+    { id: 2, name: "Mittens", breed: "Maine Coon", price: "€700" },
+    { id: 3, name: "Shadow", breed: "British Shorthair", price: "€600" }
+];
+
+module.exports = cats;
